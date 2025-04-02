77179ab5c8055018b31694e687d462b2e4ea1a70
Antonio Gomez <107006627+AGomezCuervo@users.noreply.github.com>
2025-04-01 19:29:33 -0500

    rm xkbmap and added dired fm

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -84,7 +84,13 @@ vim.opt.rtp:prepend(lazypath)
 -- remap
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", function()
+        if vim.bo.filetype == "dired" then
+                vim.cmd("DiredGoBack")
+        else
+                vim.cmd("Dired")
+        end
+end)
 
 vim.keymap.set({"n", "x"}, "<leader>y", '"+y"') --x covers all visual modes
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") --gv reselects the las selection
@@ -118,78 +124,6 @@ vim.o.smarttab = true
 
 vim.cmd("colorscheme minioding")
 
--- Fzf Plugin
--- local fzf_actions = require("fzf-lua.actions")
--- local fzf = {
---         "ibhagwan/fzf-lua",
---         config = function()
---                 vim.keymap.set("n", "<leader>pf", require('fzf-lua').files, { desc = "Fzf Files" })
---                 vim.keymap.set("n", "<leader>ps", require('fzf-lua').live_grep, { desc = "Fzf live_grep" })
---                 local actions = require "fzf-lua.actions"
---                 require'fzf-lua'.setup {
---                         winopts = {
---                                 border = false,
---                                 fullscreen = true,
---                                 preview = {
---                                         hidden = 'hidden',
---                                 },
---                         },
---
---                         grep = {
---                                 rg_opts = "--column --line-number --no-heading --color=always --smart-case --max-columns=4096 --glob '!node_modules/*' --glob '!tags' -e "
---                         },
---
---                         files = {
---                                 fd_opts = "--exclude node_modules --exclude .git",
---                         },
---
---                         keymap = {
---                                 builtin = {
---                                         false,
---                                         ["<M-Esc>"] = "hide",
---                                         ["<F1>"] = "toggle-help",
---                                         ["<F2>"] = "toggle-fullscreen",
---                                         ["<F3>"] = "toggle-preview-wrap",
---                                         ["<F4>"] = "toggle-preview",
---                                         ["<F5>"] = "toggle-preview-ccw",
---                                         ["<F6>"] = "toggle-preview-cw",
---                                         ["<S-down>"] = "preview-page-down",
---                                         ["<S-up>"] = "preview-page-up",
---                                         ["<M-S-down>"] = "preview-down",
---                                         ["<M-S-up>"] = "preview-up",
---                                 },
---                                 fzf = {
---                                         false,
---                                         ["ctrl-z"] = "abort",
---                                         ["ctrl-u"] = "unix-line-discard",
---                                         ["ctrl-f"] = "half-page-down",
---                                         ["ctrl-b"] = "half-page-up",
---                                         ["ctrl-a"] = "beginning-of-line",
---                                         ["ctrl-e"] = "end-of-line",
---                                         ["alt-a"] = "toggle-all",
---                                         ["alt-g"] = "last",
---                                         ["alt-G"] = "first",
---                                         ["f3"]= "toggle-preview-wrap",
---                                         ["f4"]= "toggle-preview",
---                                         ["shift-down"] = "preview-page-down",
---                                         ["shift-up"] = "preview-page-up",
---                                 },
---                         },
---                         actions = {
---                                 files = {
---                                         false,
---                                         ["ctrl-y"] = actions.file_edit_or_qf,
---                                         ["enter"] = actions.file_edit_or_qf,
---                                         ["ctrl-s"] = actions.file_split,
---                                         ["ctrl-v"] = actions.file_vsplit,
---                                         ["ctrl-t"] = actions.file_tabedit,
---                                         ["alt-q"] = actions.file_sel_to_qf,
---                                         ["alt-Q"] = actions.file_sel_to_ll,
---                                 },
---                         },
---                 }
---         end
--- }
 
 -- Gitsigns
 local git_signs = {
@@ -236,6 +170,22 @@ local git_signs = {
       },
     })
   end
+}
+
+-- Dired
+local dired = {
+        "X3eRo0/dired.nvim",
+        dependencies = { "MunifTanjim/nui.nvim" },
+        config = function()
+                require("dired").setup {
+                        path_separator = "/",
+                        show_banner = false,
+                        show_icons = false,
+                        show_hidden = true,
+                        show_dot_dirs = true,
+                        show_colors = true,
+                }
+        end
 }
 
 -- Treesitter Plugin
@@ -389,7 +339,7 @@ local nvim_cmp = {
 require("lazy").setup({
         spec = {
                 treesitter,
-                -- fzf,
+                dired,
                 lsp,
                 nvim_cmp,
                 git_signs,
